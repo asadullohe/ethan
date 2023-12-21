508630e602aa967a34d6209402eaa2088129a49b
Asadulloh-2002 <asadullohusanboyev56@gmail.com>
2023-12-21 18:49:40 +0500

    feat(pages): add nav

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,24 @@
-const Layout = () => {
-  return <div>Layout</div>;
+import { Sora } from "@next/font/google";
+
+const sora = Sora({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700", "800"],
+  variable: "--font-sora",
+});
+
+import Header from "../components/Header";
+import Nav from "../components/Nav";
+import TopLeftImg from "../components/TopLeftImg";
+
+const Layout = ({ children }) => {
+  return (
+    <div className={`page bg-site text-white bg-cover bg-no-repeat ${sora.variable} relative font-sora`}>
+      <TopLeftImg />
+      <Nav />
+      <Header />
+      {children}
+    </div>
+  );
 };
 
 export default Layout;
